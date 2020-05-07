f90c60edb1f5d34cdfb05e4537f200fff04e0587
jiajunliu0024 <61958672+jiajunliu0024@users.noreply.github.com>
2020-05-07 18:11:36 +1000

    delete uncessary native mode building

--- Color_Test_WPF_App_NET_Framework/Program1.cs
+++ Color_Test_WPF_App_NET_Framework/Program1.cs
@@ -116,23 +116,5 @@ namespace Color_Test_WPF_App_NET_Framework
       
     }
 
-    public class NativeMethods
-    {
-        const string Magnification = "Magnification.dll";
-
-        [DllImport(Magnification, ExactSpelling = true, SetLastError = true)]
-        public static extern bool MagInitialize();
-
-        [DllImport(Magnification, ExactSpelling = true, SetLastError = true)]
-        public static extern bool MagUninitialize();
-
-        [DllImport(Magnification, ExactSpelling = true, SetLastError = true)]
-        public static extern bool MagSetFullscreenColorEffect(ref MAGCOLOREFFECT pEffect);
-        
-        public struct MAGCOLOREFFECT
-        {
-            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 25)]
-            public float[] transform;
-        }
-    }
+   
 }
