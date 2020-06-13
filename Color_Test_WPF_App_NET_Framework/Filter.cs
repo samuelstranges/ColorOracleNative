790a4d6d91611ae2afeeb7e43e6b280a8a364932
LCHLim <43302483+LCHLim@users.noreply.github.com>
2020-06-14 00:09:17 +1000

    Included Exception (throw, try, catch) in Simulator and Live, Create Filter-interface

---
+++ Color_Test_WPF_App_NET_Framework/Filter.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Color_Test_WPF_App_NET_Framework
+{
+
+    /// <summary>
+    /// Inferface for Different kind of Simulator
+    /// </summary>
+    interface Filter
+    {
+
+        /// <summary>
+        /// Simulate diffenrent kind of color blindness vision
+        /// </summary>
+        /// <param name="inData">pixels data to be processed</param>
+        /// <returns>processed pixels data</returns>
+        int[] filter(int[] inData);
+    }
+}
